5802e80b2ff949357323f860abec345befc1c653
jhidalgom <juanda@hidalgo.es>
2018-02-12 18:26:32 +0100

    Initial Project Import

---
+++ src/main/java/com/hpe/test/mcpanalyzer/McpanalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.hpe.test.mcpanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpanalyzerApplication.class, args);
+	}
+}
